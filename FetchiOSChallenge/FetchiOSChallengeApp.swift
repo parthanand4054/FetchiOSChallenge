ea229107198f9a73cf7cbff5b6932efb730036ed
parth4054 <parth4054@gmail.com>
2024-01-30 18:29:53 -0800

    Initial Commit

---
+++ FetchiOSChallenge/FetchiOSChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchiOSChallengeApp.swift
+//  FetchiOSChallenge
+//
+//  Created by Parth Anand on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchiOSChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
